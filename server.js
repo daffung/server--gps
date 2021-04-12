dc4d4bf79f802227c126e54e975449f82810025e
daffung <65096000+daffung@users.noreply.github.com>
2021-04-12 13:24:05 +0700

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,26 @@ const io = require('socket.io')(server, {
         "optionsSuccessStatus": 204
     }
 });
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
 
+function success(pos) {
+  var crd = pos.coords;
+
+  console.log('Your current position is:');
+  console.log(`Latitude : ${crd.latitude}`);
+  console.log(`Longitude: ${crd.longitude}`);
+  console.log(`More or less ${crd.accuracy} meters.`);
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+navigator.geolocation.getCurrentPosition(success, error, options);
 io.on('connection', (socket) => {
     socket.emit('hi', {date:Date.now()})
     socket.on('sendData',(data)=>{
