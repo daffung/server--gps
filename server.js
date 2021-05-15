cb23b10639997d817edbd8af1c92ede99730ab0a
daffung <dat.phung.81099@gmail.com>
2021-05-15 22:08:01 +0700

    save history

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+  }
 
 const express = require('express')
 const app = express()
@@ -5,7 +8,29 @@ const cors = require('cors')
 const server = require('http').createServer(app)
 app.use(cors())
 
+//object for 1 item (update with array when using many items)
+let newGeoJson
+//
 //app.use(express.static(__dirname+'clients'))
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology:true })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
+const lastLocation = require('./models/lastLocation')
+
+
+
+
+app.get('/lastlocation',async (req,res)=>{
+    const existData = await lastLocation.find({})
+    res.status(200).send(existData)
+})
+
+
+
+
 
 const io = require('socket.io')(server, {
     cors: {
@@ -16,14 +41,24 @@ const io = require('socket.io')(server, {
     }
 });
 
+
+
 io.on('connection', (socket) => {
-    socket.emit('hi', 'Hello World')
+    socket.emit('hi', {date:Date.now()})
     socket.on('sendData',(data)=>{
-        console.log(data)
-       socket.broadcast.emit('renderData',{data:data})
+        newGeoJson = data
+        io.emit('renderData',{data:data.data,date:data.date})
     })
-    socket.on('disconnect',()=>{
+    socket.on('disconnect',async ()=>{
+      
+        const lastData = new lastLocation({name:JSON.parse(newGeoJson).properties.title,lastLocation:newGeoJson})
+        const savedData = await lastLocation.findOneAndUpdate({name:lastData.name},{lastLocation:lastData.lastLocation})
+        if(savedData === null){
+            await lastData.save()
+        }
+        
         console.log('Disconnected')
     })
 })
-server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
+//console.log(Date.now())
+server.listen(process.env.PORT || 4000, () => console.log(`Server has started.`));
